eb9f1b44a04aaa3dfb8d23858ab7a8b7c87d4138
AndreaSciolla <andrea.sciolla@itiscuneo.eu>
2024-02-18 16:49:09 +0100

    Create ES_PRONTO_SOCCORSO.c

---
+++ ES_PRONTO_SOCCORSO.c
@@ -0,0 +1,15 @@
+/*
+Nei Pronto Soccorsi dell’ospedale i pazienti sono classificati con un colore: 
+rosso (urgentissimo)
+giallo (grave)
+verde (moderato)
+bianco (lieve). 
+Funziona così: quando una persona arriva al pronto soccorso l’infermiere lo registra (nome e età) e decide anche il colore di priorità; appena un medico è libero deve gestire prima quelli con codice rosso, poi giallo, poi verde e poi bianco. 
+Dichiara una struttura Paziente con i campi:
+Nome,
+Età,
+Colore;
+il programma deve gestire 4 code diverse e inserire il paziente nella lista corretta, quando un medico è libero entra il paziente successivo, (quindi sceglie il primo dei pazienti codice verde solo se non ci sono codici rosso e giallo).
+Le informazioni vanno visualizzate a schermo.
+*/
+
